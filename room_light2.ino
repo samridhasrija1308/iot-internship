79c3e726429f668cfa07f42d29e3695e2c5e561a
samridhasrija1308 <samridhasrija@gmail.com>
2026-06-23 21:38:49 +0530

    Add files via upload

---
+++ room_light2.ino
@@ -0,0 +1,24 @@
+#define PIRPIN 5
+#define LED 9
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(PIRPIN, INPUT);
+  pinMode(LED, OUTPUT);
+}
+void loop()
+{
+  int l = digitalRead(PIRPIN);
+  Serial.println("Motion detect");
+  Serial.println(l);
+  if (l == 1)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
+
+  delay(1000);
+}
