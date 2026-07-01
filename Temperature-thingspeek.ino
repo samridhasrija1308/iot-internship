a5879504937b8a7b431444ec7883a46b18a09db9
samridhasrija1308 <samridhasrija@gmail.com>
2026-07-01 15:06:01 +0530

    Add files via upload

---
+++ Temperature-thingspeek.ino
@@ -0,0 +1,53 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+const char*ssid="vivo Y39 5";
+const char*password="sam@123456";
+const char*apiKey="T6VH70VAUPC7N4H3";
+const char*server="api.thingspeak.com";
+WiFiClient client;
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+WiFi.begin(ssid,password);
+Serial.println();
+Serial.print("Connecting to Wi-Fi");
+while(WiFi.status()!=WL_CONNECTED)
+{
+  delay(500);
+  Serial.print(".");
+
+}
+Serial.println("Connected");
+}
+void loop()
+{
+  if(WiFi.status()==WL_CONNECTED)
+
+{
+  HTTPClient http;
+  float sensorData=random(20,30);
+  String url="http://";
+  url+=server;
+  url+="/update?api_key=";
+  url+=apiKey;
+  url+="&field1=";
+  url+=String(sensorData);
+  http.begin(client,url);
+  int httpCode=http.GET();
+  if(httpCode>0)
+  {
+    Serial.print("Data sent! Response Code:");
+    Serial.println(httpCode);
+
+  }
+  else
+  {
+    Serial.println("Error sending data");
+
+  }
+  http.end();
+
+}
+delay(2000);
+}
